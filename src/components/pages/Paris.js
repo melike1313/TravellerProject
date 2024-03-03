e2c19063f20a2af2c9b98f65d452f154d60a53e7
melike1313 <melikenogay13@gmail.com>
2024-03-03 22:25:27 +0300

    new

--- src/components/pages/Paris.js
+++ src/components/pages/Paris.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
 import "../Paris.css";
 import { Link } from 'react-router-dom';
@@ -9,47 +9,59 @@ import louvreImage from '../../images/Louvre.jpg';
 import EyfelKulesi from '../../images/Eyfel.jpg';
 import PalaisMuze from '../../images/Palais.jpg';
 import Disney from '../../images/Disneyland.jpg';
-import { useState } from 'react';
 
 const Paris = () => {
-  const [isHovered, setIsHovered] = useState(false); // Add this line
+  const [isHovered, setIsHovered] = useState(false);
   const settings = {
-    dots: true, // Dots'ları gizlemek için
-    infinite: true, // Sonsuz döngü
+    dots: true,
+    infinite: true,
     speed: 500,
-    slidesToShow: 1, // Tek bir slide'ı göster
-    slidesToScroll: 1, // Tek bir slide'a kaydır
-    dotsClass: "slick-dots slick-dots-custom", // Özelleştirilmiş dots sınıfını ekler
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: "slick-dots slick-dots-custom",
     responsive: [
-       {
-         breakpoint: 768,
-         settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1
-         }
-       }
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
     ]
-   };
+  };
 
-
- return (
+  return (
     <div className="paris-container">
       <div className="paris-video-container">
         <video className="paris-video" src="/videos/ParisVideo.mp4" autoPlay loop muted />
       </div>
       <div className="paris-info">
         <Slider {...settings}>
-        <div className="info-box"
+          <div className="info-box"
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
           >
-            
             <Link to="/louvre">
               <img src={louvreImage} alt="Louvre Müzesi" />
             </Link>
             {isHovered && <div className="hover-text"> <p>Louvre Müzesi</p>
-                       <p>Paris, Fransa</p>
-                       <p>Dünyanın en büyük sanat müzesi</p>
+              <p>Paris, Fransa</p>
+              <p>Dünyanın en büyük sanat müzesi</p>
             </div>}
           </div>
           <div className="info-box">
@@ -64,7 +76,7 @@ const Paris = () => {
         </Slider>
       </div>
     </div>
- );
+  );
 };
 
 export default Paris;
