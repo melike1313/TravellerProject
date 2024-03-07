248e862123cd3e209a9d9d39ca860752eafef621
beyzagursoy <b.gursoy0112@gmail.com>
2024-03-08 00:32:50 +0300

    upload

--- src/components/pages/Paris.js
+++ src/components/pages/Paris.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import "../../App.css";
 import "../Paris.css";
-import { Link } from 'react-router-dom';
-import Slider from "react-slick";
+import ReactDOM from 'react-dom';
+import classNames from "classnames";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import louvreImage from '../../images/Louvre.jpg';
@@ -10,62 +10,110 @@ import EyfelKulesi from '../../images/Eyfel.jpg';
 import PalaisMuze from '../../images/Palais.jpg';
 import Disney from '../../images/Disneyland.jpg';
 
-const Paris = () => {
- const [hoveredItem, setHoveredItem] = useState(null); // Track the currently hovered item
+class Paris extends React.Component {
+  constructor(props) {
+    super(props);
 
- const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dotsClass: "slick-dots slick-dots-custom",
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
- };
+    this.IMAGE_PARTS = 4;
 
- return (
-    <div className="paris-container">
-      <div className="paris-video-container">
-        <video className="paris-video" src="/videos/ParisVideo.mp4" autoPlay loop muted />
-      </div>
-      <div className="paris-info">
-        <Slider {...settings}>
-          <div className="info-box" onMouseEnter={() => setHoveredItem('louvre')} onMouseLeave={() => setHoveredItem(null)}>
-            <Link to="/louvre">
-              <img src={louvreImage} alt="Louvre Museum" />
-            </Link>
-            {hoveredItem === 'louvre' && <div className="hover-text">Louvre Museum</div>}
-          </div>
-          <div className="info-box" onMouseEnter={() => setHoveredItem('eyfel')} onMouseLeave={() => setHoveredItem(null)}>
-            <Link to="/eyfel">
-              <img src={EyfelKulesi} alt="Eiffel Tower" />
-            </Link>
-            {hoveredItem === 'eyfel' && <div className="hover-text">Eiffel Tower</div>}
-          </div>
-          <div className="info-box" onMouseEnter={() => setHoveredItem('palais')} onMouseLeave={() => setHoveredItem(null)}>
-            <Link to="/palais">
-              <img src={PalaisMuze} alt="Palais Museum" />
-            </Link>
-            {hoveredItem === 'palais' && <div className="hover-text">Palais Museum</div>}
-          </div>
-          <div className="info-box" onMouseEnter={() => setHoveredItem('disney')} onMouseLeave={() => setHoveredItem(null)}>
-            <Link to="/disney">
-              <img src={Disney} alt="Disneyland" />
-            </Link>
-            {hoveredItem === 'disney' && <div className="hover-text">Disneyland</div>}
-          </div>
-        </Slider>
-      </div>
-    </div>
- );
-};
+    this.changeTO = null;
+    this.AUTOCHANGE_TIME = 4000;
+
+    this.state = { activeSlide: -1, prevSlide: -1, sliderReady: false };
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.changeTO);
+  }
+
+  componentDidMount() {
+    this.runAutochangeTO();
+    setTimeout(() => {
+      this.setState({ activeSlide: 0, sliderReady: true });
+    }, 0);
+  }
+
+  runAutochangeTO() {
+    this.changeTO = setTimeout(() => {
+      this.changeSlides(1);
+      this.runAutochangeTO();
+    }, this.AUTOCHANGE_TIME);
+  }
+
+  changeSlides(change) {
+    if (!this.props.slides) {
+      return;
+    } 
+    window.clearTimeout(this.changeTO);
+    const { length } = this.props.slides;
+    const prevSlide = this.state.activeSlide;
+    let activeSlide = prevSlide + change;
+    if (activeSlide < 0) activeSlide = length - 1;
+    if (activeSlide >= length) activeSlide = 0;
+    this.setState({ activeSlide, prevSlide });
+  }
+
+  render() {
+    if (!this.props.slides) {
+      return null; // veya başka bir yedek bileşen döndürebilirsiniz
+    } 
+    const { activeSlide, prevSlide, sliderReady } = this.state;
+    return /*#__PURE__*/(
+      React.createElement("div", { className: classNames('slider', { 's--ready': sliderReady }) }, /*#__PURE__*/
+      React.createElement("p", { className: "slider__top-heading" }, "Travelers"), /*#__PURE__*/
+      React.createElement("div", { className: "slider__slides" },
+      this.props.slides.map((slide, index) => /*#__PURE__*/
+      React.createElement("div", {
+        className: classNames('slider__slide', { 's--active': activeSlide === index, 's--prev': prevSlide === index }),
+        key: slide.city }, /*#__PURE__*/
+
+      React.createElement("div", { className: "slider__slide-content" }, /*#__PURE__*/
+      React.createElement("h3", { className: "slider__slide-subheading" }, slide.country || slide.city), /*#__PURE__*/
+      React.createElement("h2", { className: "slider__slide-heading" },
+      slide.city.split('')?.map(l => /*#__PURE__*/React.createElement("span", null, l))), /*#__PURE__*/
+
+      React.createElement("p", { className: "slider__slide-readmore" }, "read more")), /*#__PURE__*/
+
+      React.createElement("div", { className: "slider__slide-parts" },
+      [...Array(this.IMAGE_PARTS).fill()]?.map((x, i) => /*#__PURE__*/
+      React.createElement("div", { className: "slider__slide-part", key: i }, /*#__PURE__*/
+      React.createElement("div", { className: "slider__slide-part-inner", style: { backgroundImage: `url(${slide.img})` } }))))))), /*#__PURE__*/
+
+
+
+
+
+
+      React.createElement("div", { className: "slider__control", onClick: () => this.changeSlides(-1) }), /*#__PURE__*/
+      React.createElement("div", { className: "slider__control slider__control--right", onClick: () => this.changeSlides(1) })));
+
+
+  }}
+
+
+const slides = [
+{
+  city: 'Louvre Museum',
+  country: 'Paris',
+  img: louvreImage },
+
+{
+  city: 'Eiffel Tower',
+  img: EyfelKulesi },
+
+{
+  city: 'Palais',
+  country: 'Paris',
+  img: PalaisMuze },
+
+{
+  city: 'Disneyland',
+  country: 'Paris',
+  img: Disney },
+];
+
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(Paris, { slides: slides }), document.querySelector('#app'));
 
 export default Paris;
