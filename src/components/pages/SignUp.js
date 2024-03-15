9c0e43658ea15500ebe9fa9c4afe53327b512ba1
melike1313 <melikenogay13@gmail.com>
2024-03-15 12:48:49 +0300

    new1 git commit -mnew1

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,7 +1,40 @@
-import React from "react";
-import "../../App.css";
+import React, { useState } from 'react'
+import "../SignUp.css"
+import user_icon from "../../images/person.png"
+import email_icon from "../../images/email.png"
+import password_icon from "../../images/password.png"
 
+export const SignUp = () => {
+    const [action,setAction]=useState("Login");
+  return (
+    <div className='container'>
+          <video src="/videos/ocean.mp4" autoPlay loop muted />
+        <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            {action==="Login"?<div></div>:<div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder='Name' />
+            </div>}
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" placeholder='Mail'/>
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder='Password' />
+            </div>
+        </div>
+        {action==="Sign Up"?<div></div>:<div className="forgot-password">Lost Password? <span>Click Here</span></div>}
+        
+        <div className="submit-container">
+            <div className={action==="Login"?"submit gray":"submit"}onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"}onClick={()=>{setAction("Login")}}>Login</div>
+        </div>
+    </div>
+  )
+}
 
-export default function SignUp() {        
-    return <h1 className="sign-up">SIGN UP</h1>;
-    }       
+export default SignUp
